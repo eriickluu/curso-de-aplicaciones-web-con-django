eb68a8f896aa44401cd297c9511aee0b29cb5eec
Erick Lucio <erick.adrian.lu@gmail.com>
2020-07-13 16:21:56 -0500

    Ciclos y condicionales

--- dmh_store/dmh_store/views.py
+++ dmh_store/dmh_store/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html',{
         #context
-        'message': 'Nuevo mensaje desde la vista'
+        'message': 'Listado de productos',
+        'title': 'Productos',
+        'products': [
+            {'title': 'Playera', 'price': 5, 'stock': True}, # produto
+            {'title': 'Camisa', 'price': 7, 'stock': True},
+            {'title': 'Mochila', 'price': 20, 'stock': False}
+        ]
     })
