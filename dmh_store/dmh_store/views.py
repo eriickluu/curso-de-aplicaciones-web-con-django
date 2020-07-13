3868b355e040cdadaec9c5dfce6bf2b73656e02b
Erick Lucio <erick.adrian.lu@gmail.com>
2020-07-12 22:32:48 -0500

    Renderizar template

--- dmh_store/dmh_store/views.py
+++ dmh_store/dmh_store/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Hola mundo')
+    return render(request, 'index.html',{
+        #context
+    })
