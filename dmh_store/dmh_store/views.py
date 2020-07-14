243d7362e3ba114203f722c51c3b0710cf51cb4e
Erick Lucio <erick.adrian.lu@gmail.com>
2020-07-14 13:22:31 -0500

    logout

--- dmh_store/dmh_store/views.py
+++ dmh_store/dmh_store/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect
 
 from django.contrib import messages
 from django.contrib.auth import login
+from django.contrib.auth import logout
 from django.contrib.auth import authenticate
 
 def index(request):
@@ -34,3 +35,8 @@ def login_view(request):
     return render(request, 'users/login.html', {
 
     })
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Sesión cerrada exitosamente')
+    return redirect('login')
